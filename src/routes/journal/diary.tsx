1ac7436b3ede76d8d0b06fa8be0c4cfcaf94ae4e
Nguyen Dinh Duong <DuongNDSE183456@fpt.edu.vn>
2025-06-04 17:36:10 +0700

    fix stuff

--- src/routes/journal/diary.tsx
+++ src/routes/journal/diary.tsx
@@ -1,14 +1,17 @@
-import DiaryPage from '@/app/diary/page'
-import { createFileRoute } from '@tanstack/react-router'
+import DiaryPage from "@/app/diary/page";
+import { createFileRoute } from "@tanstack/react-router";
+import { MantineProvider } from "@mantine/core";
 
-export const Route = createFileRoute('/journal/diary')({
+export const Route = createFileRoute("/journal/diary")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
     <>
-      <DiaryPage />
+      <MantineProvider>
+        <DiaryPage />
+      </MantineProvider>
     </>
-  )
+  );
 }
